26e7c142a4cb42b506309e9a2a4fc34be6583f23
cchumacero <cnchumacero@gmail.com>
2025-03-12 19:58:46 -0300

    First commit with tests

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('test-prueba', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+  await page.getByRole('heading', { name: 'To Do List' }).click();
+  await page.getByRole('textbox', { name: 'Ingresa tu tarea...' }).click();
+  await page.getByRole('button', { name: '+' }).click();
+});
