997d89fb9f237f387e7ba9f9817a004698aeabc7
cchumacero <cnchumacero@gmail.com>
2025-03-17 11:41:57 -0300

    Fix

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('test-prueba', async ({ page }) => {
   await page.goto('http://localhost:5173');
+  await expect(page).toHaveTitle('To Do')
   await page.getByRole('heading', { name: 'To Do List' }).click();
   await page.getByRole('textbox', { name: 'Ingresa tu tarea...' }).click();
   await page.getByRole('button', { name: '+' }).click();
+  
 });
